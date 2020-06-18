293c2089275eccbe493f5da85114ff82f9e4d13c
arnoconradie <T1T1P0C0>
2020-06-18 18:34:04 +0200

    Add functions for addtion and subtraction

---
+++ calculator.py
@@ -0,0 +1,11 @@
+print('bob')
+
+'''
+Calculator libary containing basic math operations.
+'''
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
