5105135d98a99e0d173c6ecd48e477c08491acda
arnoconradie <T1T1P0C0>
2020-06-19 09:16:59 +0200

    bug fix

--- calculator.py
+++ calculator.py
@@ -1,11 +1,17 @@
+
 print('bob')
 
 '''
 Calculator libary containing basic math operations.  dddsd
 '''
 
+
 def add(first_term, second_term):
     return first_term + second_term
 
+
 def subtract(first_term, second_term):
     return first_term - second_term
+
+def multiply(first_term, second_term):
+    return first_term * second_term
